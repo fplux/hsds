c074e1da2d006dc09c94093f8186182a4b8538d5
Tim <tmc1618@gmail.com>
2016-11-04 14:57:49 -0500

    Fixed bug with Reports page not loading old events

--- src/features/app/components/reports/ReportsContainer.jsx
+++ src/features/app/components/reports/ReportsContainer.jsx
@@ -2,8 +2,8 @@ import { connect } from 'react-redux';
 import { Reports } from './Reports';
 
 const mapStateToProps = state => ({
-  events: state.data.events.events,
-  loading: state.data.events.loading,
+  events: state.data.pastEvents.events,
+  loading: state.data.pastEvents.loading,
 });
 
 export const ReportsContainer = connect(mapStateToProps)(Reports);
