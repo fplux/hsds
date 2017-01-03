b8d283234f7d951ce495b3a5285fbcd4b1c46f3a
Tim <tmc1618@gmail.com>
2017-01-03 08:36:47 -0600

    production files

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,19 @@
 import { applyMiddleware, createStore, compose } from 'redux';
-import createLogger from 'redux-logger';
+// import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
 import saga from '../features/data/saga';
-
-const loggerMiddleware = createLogger({
-  colors: {},
-  collapsed: () => true,
-});
+//
+// const loggerMiddleware = createLogger({
+//   colors: {},
+//   collapsed: () => true,
+// });
 
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = applyMiddleware(
-  loggerMiddleware,
+  // loggerMiddleware,
   thunk,
   sagaMiddleware
 );
@@ -22,7 +22,7 @@ const store = createStore(
   reducer,
   compose(
     middleware,
-    window.devToolsExtension ? window.devToolsExtension() : f => f
+    // window.devToolsExtension ? window.devToolsExtension() : f => f
   )
 );
 
